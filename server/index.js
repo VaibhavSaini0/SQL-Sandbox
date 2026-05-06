2a08678fe685feb1ab1d19909ac3c26e2f92e642
Vaibhav Siani <vaibhavsaini77786@gmail.com>
2026-05-06 13:19:16 +0530

    Fix Production Error

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-frontend-domain.vercel.app",
+      "https://sql-sandbox-plum.vercel.app/",
     ],
     credentials: true,
   })
